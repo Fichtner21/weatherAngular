9de8593d3a35eae3dbbb456ddd5ade9b1c3f8f65
Fichtner21 <ernest.fichtner@gmail.com>
2020-06-08 12:31:05 +0200

    Dodany service pogodowy, podstawowy widok, chart js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherService } from './weather.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [WeatherService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
